9933772126ade56081773311fb7df2106f166329
Artur <buzov.artur@gmail.com>
2019-07-26 17:48:01 +0300

    1 foo bar

--- src/main.rs
+++ src/main.rs
@@ -86,6 +86,15 @@ fn box_test() {
     let x: Box<i32> = Box::new(92);
 }
 
+fn foo() {
+    let a: Box<i32>;
+    a = Box::new(1);
+    bar(&*a)
+}
+
+fn bar(b: &i32) {
+}
+
 fn test(func: fn(i32) -> i32, i: i32) -> i32 {
     func(i)
 }
